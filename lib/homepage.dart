c9c7f4d269e8f9dd1923c69604ff18d158a9303a
tanjilalom <tanjil15-3079@diu.edu.bd>
2024-06-23 15:44:16 +0600

    Table Add

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,3 +1,5 @@
+import 'package:datatable/widget/data.dart';
+import 'package:datatable/widget/heading.dart';
 import 'package:flutter/material.dart';
 
 class Homepage extends StatelessWidget {
@@ -7,69 +9,111 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: DataTable(
-          columns: [
-            DataColumn(
-              label: Text("ID"),
-            ),
-            DataColumn(
-              label: Text("Name"),
-            ),
-            DataColumn(
-              label: Text("Department"),
-            ),
-          ],
-          rows: [
-            DataRow(
-              selected: true,
-              cells: [
-                DataCell(
-                  Text("01"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Table(
+                textDirection: TextDirection.ltr,
+                border: TableBorder.all(width: 1.0, color: Colors.grey),
+                children: [
+                  TableRow(
+                    children: [
+                      Heading('ID'),
+                      Heading('Name'),
+                      Heading('ID'),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      Data('01'),
+                      Data('Tanjil'),
+                      Data('CIS'),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      Data('02'),
+                      Data('Nisho'),
+                      Data('CSE'),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      Data('03'),
+                      Data('Mash'),
+                      Data('SWE'),
+                    ],
+                  ),
+                ]),
+
+            SizedBox(height: 50,),
+
+            DataTable(
+              columns: [
+                DataColumn(
+                  label: Text("ID"),
                 ),
-                DataCell(
-                  Text("Tanjil"),
+                DataColumn(
+                  label: Text("Name"),
                 ),
-                DataCell(
-                  Text("CSE"),
+                DataColumn(
+                  label: Text("Department"),
                 ),
               ],
-            ),
-            DataRow(
-              cells: [
-                DataCell(
-                  Text("02"),
+              rows: [
+                DataRow(
+                  //color: MaterialStateColor.resolveWith((states) => Colors.blue),
+                  cells: [
+                    DataCell(
+                      Text("01"),
+                    ),
+                    DataCell(
+                      Text("Tanjil"),
+                    ),
+                    DataCell(
+                      Text("CIS"),
+                    ),
+                  ],
                 ),
-                DataCell(
-                  Text("Real"),
+                DataRow(
+                  cells: [
+                    DataCell(
+                      Text("02"),
+                    ),
+                    DataCell(
+                      Text("Real"),
+                    ),
+                    DataCell(
+                      Text("CSE"),
+                    ),
+                  ],
                 ),
-                DataCell(
-                  Text("CSE"),
+                DataRow(
+                  selected: true,
+                  cells: [
+                    DataCell(
+                      Text("03"),
+                    ),
+                    DataCell(
+                      Text("Rafi"),
+                    ),
+                    DataCell(
+                      Text("SWE"),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-            DataRow(
-              cells: [
-                DataCell(
-                  Text("03"),
-                ),
-                DataCell(
-                  Text("Rafi"),
-                ),
-                DataCell(
-                  Text("CSE"),
-                ),
-              ],
-            ),
-            DataRow(
-              cells: [
-                DataCell(
-                  Text("02"),
-                ),
-                DataCell(
-                  Text("Fahim"),
-                ),
-                DataCell(
-                  Text("CSE"),
+                DataRow(
+                  cells: [
+                    DataCell(
+                      Text("02"),
+                    ),
+                    DataCell(
+                      Text("Fahim"),
+                    ),
+                    DataCell(
+                      Text("ESDM"),
+                    ),
+                  ],
                 ),
               ],
             ),
